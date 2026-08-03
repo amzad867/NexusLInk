6c6b84968ea51c861b86353d51bf681e5f9fd69b
amzad867 <amzad.ananinja@gmail.com>
2026-08-03 23:09:03 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,75 @@
-{
-  "name": "nexus-server",
-  "version": "1.0.0",
-  "description": "Nexus Link Realtime Server",
-  "main": "server.js",
-  "scripts": {
-    "start": "node server.js"
-  },
-  "dependencies": {
-    "express": "^4.18.2",
-    "ws": "^8.18.0"
-  }
-}
+const express = require("express");
+const { WebSocketServer } = require("ws");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Nexus Link Server Running");
+});
+
+
+const server = app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+});
+
+
+const wss = new WebSocketServer({
+    server
+});
+
+
+let devices = {};
+
+
+wss.on("connection", (ws) => {
+
+    console.log("Device Connected");
+
+
+    ws.on("message", (data) => {
+
+        let message = JSON.parse(data);
+
+        console.log(message);
+
+
+        if(message.type === "register"){
+
+            devices[message.deviceID] = ws;
+
+            console.log(
+                "Registered:",
+                message.deviceID
+            );
+
+        }
+
+
+        if(message.type === "send"){
+
+            let target = devices[message.target];
+
+            if(target){
+
+                target.send(
+                    JSON.stringify(message)
+                );
+
+            }
+
+        }
+
+
+    });
+
+
+    ws.on("close", () => {
+
+        console.log("Device Disconnected");
+
+    });
+
+
+});
