9185c89b1f042e8f2067a94576864fc69c91007b
amzad867 <amzad.ananinja@gmail.com>
2026-08-07 21:52:08 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -5,13 +5,18 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+
 app.get("/", (req, res) => {
     res.send("Nexus Link Server Running");
 });
 
 
 const server = app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+
+    console.log(
+        "Server running on port " + PORT
+    );
+
 });
 
 
@@ -23,109 +28,135 @@ const wss = new WebSocketServer({
 let devices = {};
 
 
-setInterval(() => {
+wss.on("connection", (ws) => {
 
-    wss.clients.forEach((client) => {
 
-        if(client.readyState === 1){
+    console.log("Device Connected");
 
-            client.ping();
-
-        }
-
-    });
-
-}, 7000);
-
-ws.on("close", (code, reason) => {
-
-    console.log(
-        "Disconnected:",
-        code,
-        reason.toString()
-    );
-
-});
 
 
     ws.on("message", (data) => {
 
-        let message = JSON.parse(data);
 
-        console.log(message);
-
-
-        if(message.type === "register"){
-
-            devices[message.deviceId] = ws;
-
-            console.log(
-                "Registered:",
-                message.deviceId
-            );
-
-        }
+        try {
 
 
-        if(message.type === "send"){
+            let message = JSON.parse(data);
 
-            let target = devices[message.target];
 
-            if(target){
+            console.log(message);
 
-                target.send(
-                    JSON.stringify(message)
+
+
+            if(message.type === "register"){
+
+
+                devices[message.deviceId] = ws;
+
+
+                console.log(
+                    "Registered:",
+                    message.deviceId
                 );
+
 
             }
 
+
+
+            if(message.type === "send"){
+
+
+                let target = devices[message.target];
+
+
+                if(target){
+
+                    target.send(
+                        JSON.stringify(message)
+                    );
+
+                }
+
+
+            }
+
+
+
+            if(message.type === "pair"){
+
+
+                let tablet = devices[message.tabletID];
+
+
+                if(tablet){
+
+
+                    ws.send(JSON.stringify({
+
+                        type:"pair_success",
+
+                        tabletID:message.tabletID
+
+                    }));
+
+
+
+                    tablet.send(JSON.stringify({
+
+                        type:"pair_request",
+
+                        phoneID:message.phoneID
+
+                    }));
+
+
+                }else{
+
+
+                    ws.send(JSON.stringify({
+
+                        type:"pair_failed",
+
+                        message:"Tablet not found"
+
+                    }));
+
+                }
+
+
+            }
+
+
+        } catch(error){
+
+
+            console.log(
+                "Message Error:",
+                error.message
+            );
+
+
         }
 
-        if(message.type === "pair"){
-
-    let tablet = devices[message.tabletID];
-
-    if(tablet){
-
-        ws.send(JSON.stringify({
-
-            type:"pair_success",
-
-            tabletID:message.tabletID
-
-        }));
-
-
-        tablet.send(JSON.stringify({
-
-            type:"pair_request",
-
-            phoneID:message.phoneID
-
-        }));
-
-    }else{
-
-        ws.send(JSON.stringify({
-
-            type:"pair_failed",
-
-            message:"Tablet not found"
-
-        }));
-
-    }
-
-}
 
     });
 
 
-    ws.on("close", () => {
 
-        console.log("Device Disconnected");
+
+    ws.on("close", (code, reason) => {
+
+
+        console.log(
+            "Device Disconnected:",
+            code,
+            reason.toString()
+        );
+
 
     });
+
 
 
 });
